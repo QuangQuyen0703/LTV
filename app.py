b6369623ad7007cb7442a2717b9ee068fa80789d
QuangQuyen0703 <155425427+QuangQuyen0703@users.noreply.github.com>
2024-01-16 19:55:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import plotly.express as px
 from io import StringIO
 
 # Function to calculate additional metrics
-def calculate_metrics(data, funded_cac_values):
+def calculate_metrics(data):
     # Assuming 'Year', 'Total Customer', 'Active Rate', 'New Customer', 'Funding Rate',
-    # 'ARPU', 'Direct Cost', 'Churn Rate' are columns in your data
+    # 'ARPU', 'Direct Cost', 'Churn Rate', 'Funded CAC' are columns in your data
 
     # Convert 'Direct Cost' to numeric
     data['Direct Cost'] = pd.to_numeric(data['Direct Cost'], errors='coerce')
@@ -28,10 +28,10 @@ def calculate_metrics(data, funded_cac_values):
     data['ltv'] = (data['ARPU'] - data['Direct Cost']) / data['Churn Rate']
 
     # Calculate LTV/CAC
-    data['ltv_cac_ratio'] = data['ltv'] / (funded_cac_values * data['new_funded_customer'])
+    data['ltv_cac_ratio'] = data['ltv'] / data['Funded CAC']
 
     # Calculate Payback
-    data['payback'] = (data['ARPU'] - data['Direct Cost']) / funded_cac_values
+    data['payback'] = (data['ARPU'] - data['Direct Cost']) / data['Funded CAC']
 
     return data
 
@@ -41,26 +41,10 @@ st.title('LTV:CAC Visualization App')
 data = pd.read_csv("./data.csv")
 st.write(data)
 
-# Allow user input for Funded CAC from 2024 to 2028
-funded_cac_2024 = st.number_input('Enter Funded CAC for 2024', min_value=0.0)
-funded_cac_2025 = st.number_input('Enter Funded CAC for 2025', min_value=0.0)
-funded_cac_2026 = st.number_input('Enter Funded CAC for 2026', min_value=0.0)
-funded_cac_2027 = st.number_input('Enter Funded CAC for 2027', min_value=0.0)
-funded_cac_2028 = st.number_input('Enter Funded CAC for 2028', min_value=0.0)
-
-# Store user input in a dictionary
-funded_cac_values = {
-    2024: funded_cac_2024,
-    2025: funded_cac_2025,
-    2026: funded_cac_2026,
-    2027: funded_cac_2027,
-    2028: funded_cac_2028
-}
-
 # Check if data is available and then process it
 if 'data' in locals() and not data.empty:
     # Process and calculate additional metrics
-    processed_data = calculate_metrics(data, funded_cac_values)
+    processed_data = calculate_metrics(data)
     
     # Visualization
     st.subheader('Additional Metrics Visualization')
