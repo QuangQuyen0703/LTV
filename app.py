d5ca3ee6fa1d34f27dd6c97747a7f1d4a2a733c9
QuangQuyen0703 <155425427+QuangQuyen0703@users.noreply.github.com>
2024-01-17 08:47:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
-import matplotlib.pyplot as plt  # Import matplotlib for creating column chart
+import matplotlib.pyplot as plt
 
 # Function to calculate additional metrics
 def calculate_metrics(data, funded_cac_increase):
@@ -116,27 +116,24 @@ if 'data' in locals() and not data.empty:
     st.plotly_chart(fig_payback_chart)
 
     # Column chart for Total Gross Profit by year
-st.subheader('Total Gross Profit')
-fig, ax = plt.subplots()
-bars = ax.bar(processed_data['Year'], processed_data['total_gross_profit'])
+    st.subheader('Total Gross Profit')
+    fig, ax = plt.subplots()
+    bars = ax.bar(processed_data['Year'], processed_data['total_gross_profit'])
 
-# Displaying labels on top of the bars
-for bar in bars:
-    yval = bar.get_height()
-    plt.text(bar.get_x() + bar.get_width()/2, yval, round(yval, 2), ha='center', va='bottom')
+    # Displaying labels on top of the bars
+    for bar in bars:
+        yval = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width()/2, yval, round(yval, 2), ha='center', va='bottom')
 
-st.pyplot(fig)
+    st.pyplot(fig)
 
-# Column chart for Gross Profit from 2021 to 2028
-st.subheader('Gross Profit (2021-2028)')
-fig_gross_profit = go.Figure()
-fig_gross_profit.add_trace(go.Bar(x=processed_data['Year'], y=processed_data['total_gross_profit']))
+    # Column chart for Gross Profit from 2021 to 2028
+    st.subheader('Gross Profit (2021-2028)')
+    fig_gross_profit = go.Figure()
+    fig_gross_profit.add_trace(go.Bar(x=processed_data['Year'], y=processed_data['total_gross_profit']))
 
-fig_gross_profit.update_layout(title='Gross Profit from 2021 to 2028')
-st.plotly_chart(fig_gross_profit)
-
-
-   
+    fig_gross_profit.update_layout(title='Gross Profit from 2021 to 2028')
+    st.plotly_chart(fig_gross_profit)
 
     # Additional insights
     st.subheader('Insights')
