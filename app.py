f9aa71793024ff48bde78d5101725a8282ca50f7
QuangQuyen0703 <155425427+QuangQuyen0703@users.noreply.github.com>
2024-01-18 11:58:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -112,18 +112,18 @@ if 'data' in locals() and not data.empty:
     st.plotly_chart(fig_funded_cac_ltv_column)
 
     # Line chart for LTV/CAC by year
-fig_line_chart = go.Figure()
+    fig_line_chart = go.Figure()
 
-# Add LTV/CAC to the line chart with red color
-fig_line_chart.add_trace(go.Scatter(x=processed_data['Year'], y=processed_data['ltv_cac_ratio'],
-                                   mode='lines+text', name='LTV/CAC Ratio', line=dict(color='#EB3300'),
-                                   text=processed_data['ltv_cac_ratio'].round(2),
-                                   textposition='top left', textfont=dict(color='#7F7F7F')))
+    # Add LTV/CAC to the line chart with red color
+    fig_line_chart.add_trace(go.Scatter(x=processed_data['Year'], y=processed_data['ltv_cac_ratio'],
+                                       mode='lines+text', name='LTV/CAC Ratio', line=dict(color='#EB3300'),
+                                       text=processed_data['ltv_cac_ratio'].round(2),
+                                       textposition='top left', textfont=dict(color='#7F7F7F')))
 
-fig_line_chart.update_layout(title='LTV/Funded CAC Ratio')
-fig_line_chart.update_xaxes(showgrid=False)  # Remove x-axis gridlines
-fig_line_chart.update_yaxes(showgrid=False)  # Remove y-axis gridlines
+    fig_line_chart.update_layout(title='LTV/Funded CAC Ratio')
+    fig_line_chart.update_xaxes(showgrid=False)  # Remove x-axis gridlines
+    fig_line_chart.update_yaxes(showgrid=False)  # Remove y-axis gridlines
 
-st.plotly_chart(fig_line_chart)
-st.title('Thank You')
+    st.plotly_chart(fig_line_chart)
+    st.title('Thank You')
 
